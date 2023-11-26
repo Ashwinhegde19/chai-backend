bee30965eb83b5c7f015f755cf0d747c80f7509c
Ashwinhegde19 <ashwinhegde19@gmail.com>
2023-11-26 12:01:19 +0530

    fixed register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,7 +9,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // get user details from frontend
     const {fullName, email, username, password } = req.body
-    console.log("email: ", email);
+    
 
     // validation - not empty
     if (
@@ -28,16 +28,14 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
 
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    // let coverImageLocalPath;
-    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    //     coverImageLocalPath = req.files.coverImage[0].path
-    // }
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
